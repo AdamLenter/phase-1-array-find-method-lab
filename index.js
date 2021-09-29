8ab1377be2cc5def8f47811828aa5cdbb838d016
Adam <adamlenter1@gmail.com>
2021-09-29 13:06:34 -0400

    commit tree 5378171ff40548bda778ed8986931e80e6890f61

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+
+  function isWin(element) {
+      if(element.result === "W") {
+          return element;
+      }
+  }
+
+  function superbowlWin(array) {
+      let arrayRow = array.find(isWin);
+
+      if (arrayRow !== undefined) {
+          return arrayRow.year;
+      }
+      else {
+          return undefined;
+      }
+  }
